baa92849f85e89cfd58f59e4d2177b2b662a7328
EdMass <edwar.castano@sofka.com.co>
2022-06-15 14:40:40 -0500

    refactoring y excepciones

--- src/main/java/com/co/sofka/resource/AcepTyCResourse.java
+++ src/main/java/com/co/sofka/resource/AcepTyCResourse.java
@@ -1,7 +1,6 @@
 package com.co.sofka.resource;
 
 import com.co.sofka.dto.AcepTyCDto;
-import com.co.sofka.entity.AcepTyCEntity;
 import com.co.sofka.services.AcepTyCServices;
 import io.smallrye.mutiny.Uni;
 import org.jetbrains.annotations.NotNull;
@@ -13,11 +12,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE;
 
 @Path("/terms")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -27,23 +23,11 @@ public class AcepTyCResourse {
     @Inject
     AcepTyCServices acepTyCServices;
 
-    private final LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Etc/UTC") );
-
     @POST
     @Path("/aceptar")
     @Produces(APPLICATION_JSON)
     public Uni<Response> createAceptTyC(@NotNull AcepTyCDto acepTyCDto){
-        if(acepTyCDto.getTipoDocumento().equals("Cedula")){
-            AcepTyCEntity acepTyCEntity = new AcepTyCEntity(acepTyCDto.getTipoDocumento(),
-                    acepTyCDto.getDocumento(), acepTyCDto.getVersionAcep(), dateTime);
-            return acepTyCServices.addCedulaAceptTyC(acepTyCEntity).map(acep -> Response.ok(acep).build());
-        } else if (acepTyCDto.getTipoDocumento().equals("Passport")) {
-            AcepTyCEntity acepTyCEntity = new AcepTyCEntity(acepTyCDto.getTipoDocumento(),
-                    acepTyCDto.getDocumento(), acepTyCDto.getVersionAcep(), dateTime);
-            return acepTyCServices.addPassportAceptTyC(acepTyCEntity).map(acep -> Response.ok(acep).build());
-        }else {
-            return Uni.createFrom().item(Response.status(NOT_ACCEPTABLE).build());
-        }
+            return acepTyCServices.createAceptTyC(acepTyCDto);
     }
 
 }
--- src/main/java/com/co/sofka/services/AcepTyCServices.java
+++ src/main/java/com/co/sofka/services/AcepTyCServices.java
@@ -1,20 +1,31 @@
 package com.co.sofka.services;
 
+import com.co.sofka.dto.AcepTyCDto;
 import com.co.sofka.entity.AcepTyCEntity;
 import com.co.sofka.repository.AcepTyCRepository;
 import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.jetbrains.annotations.NotNull;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
+import static javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE;
 
 @ApplicationScoped
 public class AcepTyCServices {
 
     @Inject
     AcepTyCRepository acepTyCRepository;
+
+    private final LocalDateTime dateTime = LocalDateTime.now(ZoneId.of("Etc/UTC") );
 
     public boolean validationCedula(String document){
         Pattern pattern = Pattern.compile("[0-9]{2}-[PN]{2}-[0-9]{3}-[0-9]{4}");
@@ -41,6 +52,28 @@ public class AcepTyCServices {
             return acepTyCRepository.persist(acepTyCEntity);
         }
         throw new IllegalArgumentException();
+    }
+
+    @Fallback(fallbackMethod = "createFallbackMethod")
+    @Retry(maxRetries = 4)
+    public Uni<Response> createAceptTyC(@NotNull AcepTyCDto acepTyCDto){
+        String TIPO_CEDULA = "Cedula";
+        String TIPO_PASSPORT = "Passport";
+        if(acepTyCDto.getTipoDocumento().equals(TIPO_CEDULA)){
+            AcepTyCEntity acepTyCEntity = new AcepTyCEntity(acepTyCDto.getTipoDocumento(),
+                    acepTyCDto.getDocumento(), acepTyCDto.getVersionAcep(), dateTime);
+            return addCedulaAceptTyC(acepTyCEntity).map(accept -> Response.ok(accept).build());
+        } else if (acepTyCDto.getTipoDocumento().equals(TIPO_PASSPORT)) {
+                AcepTyCEntity acepTyCEntity = new AcepTyCEntity(acepTyCDto.getTipoDocumento(),
+                        acepTyCDto.getDocumento(), acepTyCDto.getVersionAcep(), dateTime);
+                return addPassportAceptTyC(acepTyCEntity).map(accept -> Response.ok(accept).build());
+        } else {
+                return Uni.createFrom().item(Response.status(NOT_ACCEPTABLE).build());
+        }
+    }
+
+    public Uni<Response> createFallbackMethod(){
+        return Uni.createFrom().item(Response.status(SERVICE_UNAVAILABLE).build());
     }
 
 }
